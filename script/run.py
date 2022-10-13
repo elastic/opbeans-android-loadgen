af6e4633f790af136e4f4619d80e4b69bf300d65
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-13 10:38:24 +0200

    Using relative paths for zip

--- script/run.py
+++ script/run.py
@@ -64,7 +64,7 @@ def build_binaries(args):
 
     run_command(command, "./opbeans-android")
     run_command(
-        "zip opbeans-android-app.zip ./opbeans-android/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk ./opbeans-android/build/outputs/apk/debug/app-debug.apk")
+        "zip opbeans-android-app.zip opbeans-android/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk opbeans-android/build/outputs/apk/debug/app-debug.apk")
 
 
 def none_or_str(value):
